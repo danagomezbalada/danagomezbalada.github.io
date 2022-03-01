d64b661f48ae4aaaa11364e2f57b7badd36fc2e5
Dana <danagomezbalada@gmail.com>
2022-03-01 14:55:32 +0100

    finished improvements, animated menu

--- js/script.js
+++ js/script.js
@@ -36,80 +36,80 @@ $(document).ready(function () {
         }
     });
 
-    // Onclick function for the language selector options
-    function languageOnClick() {
-        var selectItems = this.parentNode.parentNode.getElementsByTagName("select")[0];
-        var selected = this.parentNode.previousSibling;
-
-        for (i = 0; i < selectItems.length; i++) {
-            if (selectItems.options[i].innerHTML == this.innerHTML) {
-                selectItems.selectedIndex = i;
-                selected.innerHTML = this.innerHTML;
-                break;
-            }
-        }
-
-        selected.click();
-        $('#lang-switch').val($('#lang-switch').val()).change();
-    }
+    
     // Function that loads the custom language selector
     function loadLanguageSelector() {
         var customSelect = document.getElementsByClassName("custom-select");
         for (i = 0; i < customSelect.length; i++) {
-            var selElmnt = customSelect[i].getElementsByTagName("select")[0];
-            var elementLength = selElmnt.length;
+            var selectElements = customSelect[i].getElementsByTagName("select")[0];
 
-            /*for each element, create a new DIV that will act as the selected item:*/
+            // CreateS a new DIV that will act as the selected item
             var DIVselected = document.createElement("DIV");
             DIVselected.setAttribute("class", "select-selected");
-            DIVselected.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+            DIVselected.id = "selected";
+            DIVselected.innerHTML = selectElements.options[selectElements.selectedIndex].innerHTML;
             customSelect[i].appendChild(DIVselected);
 
-            /*for each element, create a new DIV that will contain the option list:*/
+            // Creates a new DIV that will contain the option list
             var DIVlist = document.createElement("DIV");
             DIVlist.setAttribute("class", "select-items select-hide");
-
-            for (j = 1; j < elementLength; j++) {
+            for (j = 0; j < selectElements.length; j++) {
                 // Creates a DIV for each option
-                
                 var DIVoption = document.createElement("DIV");
-                DIVoption.innerHTML = selElmnt.options[j].innerHTML;
+                DIVoption.innerHTML = selectElements.options[j].innerHTML;
+                DIVoption.id = selectElements.options[j].value;
 
-                DIVoption.addEventListener("click", languageOnClick);
+                DIVoption.addEventListener("click", function() {
+                    var selectItems = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                    var selected = this.parentNode.previousSibling;
+
+                    for (i = 0; i < selectItems.length; i++) {
+                        if (selectItems.options[i].innerHTML == this.innerHTML) {
+                            selectItems.selectedIndex = i;
+                            selected.innerHTML = this.innerHTML;
+                            var y = this.parentNode.getElementsByClassName("same-as-selected");
+                            var yl = y.length;
+                            for (k = 0; k < yl; k++) {
+                                y[k].removeAttribute("class");
+                            }
+                            this.setAttribute("class", "same-as-selected");
+                            break;
+                        }
+                    }
+
+                    selected.click();
+                    $('#lang-switch').val($('#lang-switch').val()).change();
+                });
                 DIVlist.appendChild(DIVoption);
             }
 
             customSelect[i].appendChild(DIVlist);
-
-            DIVselected.addEventListener("click", function(e) {
-                /*when the select box is clicked, close any other select boxes,
-                and open/close the current select box:*/
-                e.stopPropagation();
+            
+            DIVselected.addEventListener("click", function(event) {
+                // Open/close this select box and close other select boxes
+                event.stopPropagation();
                 closeAllSelect(this);
                 this.nextSibling.classList.toggle("select-hide");
                 this.classList.toggle("select-arrow-active");
             });
         }
     }
-    // Function closes the select boxes except the chosen one
+    // Function that closes the select boxes except the chosen one
     function closeAllSelect(elmnt) {
         var hideIDs = [];
         var selectItems = document.getElementsByClassName("select-items");
         var selectedItems = document.getElementsByClassName("select-selected");
         
         for (i = 0; i < selectedItems.length; i++) {
-            if (elmnt == selectedItems[i]) {
+            if (elmnt == selectedItems[i])
                 hideIDs.push(i)
-            }
-            else {
+            else
                 selectedItems[i].classList.remove("select-arrow-active");
-            }
         }
 
         for (i = 0; i < selectItems.length; i++) {
-            if (hideIDs.indexOf(i)) {
+            if (hideIDs.indexOf(i))
                 selectItems[i].classList.add("select-hide");
-            }
         }
     }
 
@@ -123,9 +123,6 @@ $(document).ready(function () {
 
     // Function to run on page load
     $(document).ready(function () {
-        loadLanguageSelector();
-        document.addEventListener("click", closeAllSelect);
-
         // Set colors
         var colorsCookie = getCookie('colors');
         if (colorsCookie){
@@ -151,10 +148,11 @@ $(document).ready(function () {
 
         // Set language
         $('[lang]').hide();
+        var language = "en";
         var langCookie = getCookie('lang');
         if (langCookie)
-            $('#lang-switch').val(langCookie).change();
-        else{
+            language = langCookie;
+        else {
             const langs = [];
             $('#lang-switch option').each(function() {
                 langs.push($(this).val());
@@ -162,9 +160,26 @@ $(document).ready(function () {
             
             var current = navigator.language.split('-')[0];
             if (langs.includes(current))
-                $('#lang-switch').val(current).change();
-            else
-                $('#lang-switch').val("en").change();
+                language = current;
         }
+        $('#lang-switch').val(language).change();
+
+        loadLanguageSelector();
+        $('#' + language).addClass("same-as-selected");
+        $('#select-container').hover(function () {
+            if ($('#selected').next().hasClass("select-hide")){
+                $('#selected').click();
+            }
+        }, function () {
+            if (!$('#selected').next().hasClass("select-hide")) {
+                $('#selected').click();
+            }
+        });
+
+        document.addEventListener("click", closeAllSelect);
     });
 });
+
+// TODO: Add scroll to top button?
+// TODO: Add accordions for extra text in experience and education
+// TODO: Refactor everything to jQuery
