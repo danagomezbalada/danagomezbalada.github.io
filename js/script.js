b4fa97f5b6c8b7cd708cda07630629fe29777d04
Dana <danagomezbalada@gmail.com>
2022-03-01 12:01:16 +0100

    Changed lang select, added hover for theme toggle

--- js/script.js
+++ js/script.js
@@ -36,6 +36,83 @@ $(document).ready(function () {
         }
     });
 
+    // Onclick function for the language selector options
+    function languageOnClick() {
+        var selectItems = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        var selected = this.parentNode.previousSibling;
+
+        for (i = 0; i < selectItems.length; i++) {
+            if (selectItems.options[i].innerHTML == this.innerHTML) {
+                selectItems.selectedIndex = i;
+                selected.innerHTML = this.innerHTML;
+                break;
+            }
+        }
+
+        selected.click();
+        $('#lang-switch').val($('#lang-switch').val()).change();
+    }
+    // Function that loads the custom language selector
+    function loadLanguageSelector() {
+        var customSelect = document.getElementsByClassName("custom-select");
+        for (i = 0; i < customSelect.length; i++) {
+            var selElmnt = customSelect[i].getElementsByTagName("select")[0];
+            var elementLength = selElmnt.length;
+
+            /*for each element, create a new DIV that will act as the selected item:*/
+            var DIVselected = document.createElement("DIV");
+            DIVselected.setAttribute("class", "select-selected");
+            DIVselected.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+            customSelect[i].appendChild(DIVselected);
+
+            /*for each element, create a new DIV that will contain the option list:*/
+            var DIVlist = document.createElement("DIV");
+            DIVlist.setAttribute("class", "select-items select-hide");
+
+            for (j = 1; j < elementLength; j++) {
+                // Creates a DIV for each option
+                
+                var DIVoption = document.createElement("DIV");
+                DIVoption.innerHTML = selElmnt.options[j].innerHTML;
+
+                DIVoption.addEventListener("click", languageOnClick);
+                DIVlist.appendChild(DIVoption);
+            }
+
+            customSelect[i].appendChild(DIVlist);
+
+            DIVselected.addEventListener("click", function(e) {
+                /*when the select box is clicked, close any other select boxes,
+                and open/close the current select box:*/
+                e.stopPropagation();
+                closeAllSelect(this);
+                this.nextSibling.classList.toggle("select-hide");
+                this.classList.toggle("select-arrow-active");
+            });
+        }
+    }
+    // Function closes the select boxes except the chosen one
+    function closeAllSelect(elmnt) {
+        var hideIDs = [];
+        var selectItems = document.getElementsByClassName("select-items");
+        var selectedItems = document.getElementsByClassName("select-selected");
+        
+        for (i = 0; i < selectedItems.length; i++) {
+            if (elmnt == selectedItems[i]) {
+                hideIDs.push(i)
+            }
+            else {
+                selectedItems[i].classList.remove("select-arrow-active");
+            }
+        }
+
+        for (i = 0; i < selectItems.length; i++) {
+            if (hideIDs.indexOf(i)) {
+                selectItems[i].classList.add("select-hide");
+            }
+        }
+    }
+
     // When user selects a new language
     $('#lang-switch').change(function () {
         var lang = $(this).val();
@@ -46,6 +123,9 @@ $(document).ready(function () {
 
     // Function to run on page load
     $(document).ready(function () {
+        loadLanguageSelector();
+        document.addEventListener("click", closeAllSelect);
+
         // Set colors
         var colorsCookie = getCookie('colors');
         if (colorsCookie){
@@ -87,88 +167,4 @@ $(document).ready(function () {
                 $('#lang-switch').val("en").change();
         }
     });
-
-
-
-    var i, j, l, ll, selElmnt, a, b, c;
-    var customSelect = document.getElementsByClassName("custom-select");
-    l = customSelect.length;
-    for (i = 0; i < l; i++) {
-        selElmnt = customSelect[i].getElementsByTagName("select")[0];
-        ll = selElmnt.length;
-        /*for each element, create a new DIV that will act as the selected item:*/
-        a = document.createElement("DIV");
-        a.setAttribute("class", "select-selected");
-        a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-        customSelect[i].appendChild(a);
-        /*for each element, create a new DIV that will contain the option list:*/
-        b = document.createElement("DIV");
-        b.setAttribute("class", "select-items select-hide");
-        for (j = 1; j < ll; j++) {
-            /*for each option in the original select element,
-            create a new DIV that will act as an option item:*/
-            c = document.createElement("DIV");
-            c.innerHTML = selElmnt.options[j].innerHTML;
-            c.addEventListener("click", function() {
-                /*when an item is clicked, update the original select box,
-                and the selected item:*/
-                var y, i, k, s, h, sl, yl;
-                s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-                sl = s.length;
-                h = this.parentNode.previousSibling;
-                for (i = 0; i < sl; i++) {
-                if (s.options[i].innerHTML == this.innerHTML) {
-                    s.selectedIndex = i;
-                    h.innerHTML = this.innerHTML;
-                    y = this.parentNode.getElementsByClassName("same-as-selected");
-                    yl = y.length;
-                    for (k = 0; k < yl; k++) {
-                        y[k].removeAttribute("class");
-                    }
-                    this.setAttribute("class", "same-as-selected");
-                    break;
-                }
-                }
-                h.click();
-                $('#lang-switch').val($('#lang-switch').val()).change();
-            });
-            b.appendChild(c);
-        }
-        customSelect[i].appendChild(b);
-        a.addEventListener("click", function(e) {
-            /*when the select box is clicked, close any other select boxes,
-            and open/close the current select box:*/
-            e.stopPropagation();
-            closeAllSelect(this);
-            this.nextSibling.classList.toggle("select-hide");
-            this.classList.toggle("select-arrow-active");
-        });
-    }
-    function closeAllSelect(elmnt) {
-        /*a function that will close all select boxes in the document,
-        except the current select box:*/
-        var x, y, i, xl, yl, arrNo = [];
-        x = document.getElementsByClassName("select-items");
-        y = document.getElementsByClassName("select-selected");
-        xl = x.length;
-        yl = y.length;
-        for (i = 0; i < yl; i++) {
-            if (elmnt == y[i]) {
-            arrNo.push(i)
-            } else {
-            y[i].classList.remove("select-arrow-active");
-            }
-        }
-        for (i = 0; i < xl; i++) {
-            if (arrNo.indexOf(i)) {
-            x[i].classList.add("select-hide");
-            }
-        }
-    }
-    /*if the user clicks anywhere outside the select box,
-    then close all select boxes:*/
-    document.addEventListener("click", closeAllSelect);
-
-
-
 });
